9a61485db41806e9b73546f56a012fd53a22c8c0
N Young <60322086+nai-young@users.noreply.github.com>
2020-08-24 14:42:25 +0100

    api tasks

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.render('index', { data: null, error: 'Enter a name'})
+  res.render('index', { data: null, error: null})
 })
 app.post('/', (req, res) => {
   let city = req.body.city
